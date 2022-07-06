48804cf8b2d91722aab68149c049c0dec7a80d61
Jothir Adithyan <adithyanjothir@gmail.com>
2022-07-06 21:15:54 +0530

    Add components and auth but not finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useEffect} from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {FiSettings} from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-function App() {
+import {Navbar,Sidebar,Footer,ThemeSettings} from './components'
+import {Employees,Calendar,Customers} from './pages'
+import './App.css';
+import {useState, useStateContext} from './context/ContextProvider';
+
+export const App = () => {
+    const { activeMenu } = useStateContext();
+
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <BrowserRouter>
+    <div className="felx relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{zindex:'1000'}}>
+            <TooltipComponent content="Settings" position="Top">
+                <button className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+                style={{background:"blue", borderRadius:"50%"}}>
+                    <FiSettings></FiSettings>
+                </button>
+            </TooltipComponent>
+        </div>
+        {activeMenu?  (
+            <div className='w-72 fixed sidebar
+            bg-secondary-dark-bg bg-white'><Sidebar/></div>
+        ):(
+            <div className='w-0 dark:bg-secondary-dark-bg'><Sidebar/>
+        </div>
+        )}
+        <div className={`dark:bg-main-bg bg-main-bg
+        min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+            <div className="fixed md:static bg-main-bg
+            dark:bg-main-dark-bg navbar w-full">
+                <Navbar/>
+            </div>
+        </div> 
+        <div>
+            <Routes>
+                <Route path="/" element={<Employees />}/>
+                {/* <Route path="/registration" element={<Registration />}/>
+                <Route path="/assignment" element={<Assignment />}/>
+                <Route path="/views" element={<Views />}/>
+                <Route path="/purchase" element={<Purchase />}/> */}
+            </Routes>
+        </div>
+        
     </div>
-  );
+    </BrowserRouter>
+    </div>
+     
+  )
 }
 
 export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,3 @@
+export { default as Calendar } from './Calendar';
+export { default as Customers } from './Customers';
+export { default as Employees } from './Employees';
